375d266e8be32ca416f794ae6c62837dd9b13075
Douglas-Lanny <51864361+Douglas-Lanny@users.noreply.github.com>
2023-05-18 08:52:10 -0500

    cambios iniciales

---
+++ src/test/java/com/edlas/com/edlasapp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edlas.com.edlasapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
